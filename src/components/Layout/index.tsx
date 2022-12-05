772d273d98ccd24c7dcf4b4473ea2db2bfb107e0
zmr <zengmeirong@163.com>
2022-12-05 15:56:04 +0800

    fix： 登录态失效

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout } from 'antd';
+import { useWeb3React } from '@web3-react/core';
 import { AppBar } from '../AppBar';
 import { Footer } from '../Footer';
 import { DomainLink } from '../DomainLink';
 import useWindowDimensions from '../../utils/layout';
 const { Header, Content } = Layout;
+import { injected } from '../../utils/utils';
+
 import './index.scss';
 
 export const AppLayout = React.memo((props: any) => {
   const { width } = useWindowDimensions();
+  const { account, activate, active } = useWeb3React();
+
+  useEffect(()=> {
+    if (!active) {
+      activate(injected)
+    }
+  }, [])
   return (
     <div>
       <Layout id={'main-layout'}>
