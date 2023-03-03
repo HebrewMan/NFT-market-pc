8cf2d446a264379abfd42d2cf01046b2548e569c
zmr <zengmeirong@163.com>
2023-03-03 10:37:04 +0800

    fix：--

--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -40,7 +40,7 @@ module.exports = merge(common, {
           compress: {
             warnings: false,
             drop_debugger: true,
-            drop_console: true,
+            drop_console: false,
             pure_funcs: ['console.log', 'console.error'],
           },
         },
